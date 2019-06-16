7d6cb01d8784a5d3940b234acb01687ac6208494
Mahesh-Bhatt <bhattmahesh2k@gmail.com>
2019-06-16 15:49:48 +0545

    string added

---
+++ string_example.py
@@ -0,0 +1,15 @@
+text="Nepal"
+c=text[0]
+print("First character",c)
+c=text[1]
+print("Second Character",c)
+c=text[-1]
+print("Last character",c)
+c=text[0:]
+print(c)
+c=text[len(text)-1]
+print(c)
+c=text[len(text)-2]
+print(c)
+c=[len(text)]
+print(c)
